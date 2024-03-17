d45fca31cf4c5fb4d2048c69ff7a529bc1d13e6f
mex7179 <156093026+mex7179@users.noreply.github.com>
2024-03-17 20:42:35 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = "29403305"
 api_hash = "54c435573d0f2d9dada270a0cb493b36"
-bot_token = "6832001724:AAGWu0pPbK7eC3Fyt8bLuaJiw6lSE6Bahdc"
+bot_token = "6668078100:AAFxvAHGsuAzixqt4Kv9efKZsm54buAcXxk"
 

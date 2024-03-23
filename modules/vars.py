01edd46b01b41bbb9507eb7cc0e23d44cd3eb273
akkugithub1 <157046264+akkugithub1@users.noreply.github.com>
2024-03-23 10:26:51 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = "29403305"
 api_hash = "54c435573d0f2d9dada270a0cb493b36"
-bot_token = "6668078100:AAFxvAHGsuAzixqt4Kv9efKZsm54buAcXxk"
+bot_token = "7040476763:AAG0lFighTJWCzbjvreUAxHWug_gNyLBmOA"
 
